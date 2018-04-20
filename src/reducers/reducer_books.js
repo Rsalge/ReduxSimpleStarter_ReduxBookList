efa058aa664c52ed0c9e55c484e08101f83ad151
Ross Salge <rossnsalge@gmail.com>
2018-04-19 19:19:29 -0500

    End of 4.42: Sets up simple container component Booklist which diplays books from reducer-books via connect()

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JS the good parts" },
+    { title: "Harray Potter" },
+    { title: "The Dark tower" },
+    { title: "Eloquent Ruby" }
+  ];
+}
